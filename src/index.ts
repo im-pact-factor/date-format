e6370e0162c1eb3f89606010c36368e3271d1b38
Impact <Impact@fanruan.com>
2023-08-29 14:37:58 +0800

    feat:完成

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,41 @@ export function formate(
   formatter: string | ((dateInfo: DateInfo) => string),
   isPad = false
 ): string {
-  return "";
+  const getPad = (value: number): string => {
+    return isPad && value < 10 ? "0" + value : "" + value;
+  };
+
+  const dateInfo: DateInfo = {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate(),
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds(),
+    yyyy: "" + date.getFullYear(),
+    MM: getPad(date.getMonth() + 1),
+    dd: getPad(date.getDate()),
+    hh: getPad(date.getHours()),
+    mm: getPad(date.getMinutes()),
+    ss: getPad(date.getSeconds()),
+  };
+
+  if (typeof formatter === "function") {
+    return formatter(dateInfo);
+  }
+
+  const formatMap: { [key: string]: string } = {
+    "date": "yyyy-MM-dd",
+    "datetime": "yyyy-MM-dd hh:mm:ss"
+  };
+
+  const formatPattern = formatMap[formatter] || formatter;
+
+  return formatPattern
+    .replace("yyyy", dateInfo.yyyy)
+    .replace("MM", dateInfo.MM)
+    .replace("dd", dateInfo.dd)
+    .replace("hh", dateInfo.hh)
+    .replace("mm", dateInfo.mm)
+    .replace("ss", dateInfo.ss);
 }
